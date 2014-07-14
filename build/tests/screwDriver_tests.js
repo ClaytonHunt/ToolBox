11c52053858614527dbe1d332356b6bc1ccf9559
Clayton Hunt <claytonhunt_104@hotmail.com>
2014-07-14 16:56:09 -0400

    Got karma and linting working, still working out some of the bugs with the recognizing of test objects

---
+++ build/tests/screwDriver_tests.js
@@ -0,0 +1,15 @@
+/* global, jasmine, describe, it, expect */
+
+(function (describe, it, expect) {
+    'use strict';
+
+    describe('ScrewDriver', function() {
+
+        describe('In General', function() {
+            it('Exists', function() {
+                expect(1).toBe(1);
+            });
+        });
+    });
+
+}(describe, it, expect));
